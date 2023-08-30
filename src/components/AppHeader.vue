d0d342f638ca058d4139a42fbaff26e5e74f46ca
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-30 17:05:13 +0200

    added AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<template >
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name:'AppHeader'
+}
+</script>
+<style lang="scss" scoped>
+     @use '../styles/partials/mixins.scss' as*;
+     @use '../styles/partials/variables.scss' as*; 
+</style>
