a2ee733616134067b6cb7f52e000aa632f4d5f1a
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-30 17:04:52 +0200

    added components in App.vue and html structure

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+  import AppFooter from './components/AppFooter.vue';
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+
+  export default{
+     name:'App',
+     components:{
+        AppHeader,
+        AppFooter,
+        AppMain
+     }
+  }
+</script>
+
+<template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+<style lang="scss">
+    @import '../src/styles/general.scss'
+
+
+</style>
