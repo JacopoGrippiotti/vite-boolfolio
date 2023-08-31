a3dd7de2f2b3c72d0e96e414529b4e38906ac9b6
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-31 16:45:29 +0200

    added content to AppHeader.vue component

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,14 +1,58 @@
-<template >
-    <div>
-        
-    </div>
+<template>
+    <nav>
+        <ul>
+            <li v-for="link in links">
+                <router-link :to="{ name: link.route }">
+                    {{ link.name }}
+                </router-link>
+            </li>
+        </ul>
+    </nav>
 </template>
 <script>
 export default {
-    name:'AppHeader'
+    name: 'AppHeader',
+    data() {
+        return {
+            links : [
+                {
+                    route: 'homepage',
+                    name: 'Homepage'
+                },
+                {
+                    route: 'about',
+                    name: 'About Us'
+                },
+                {
+                    route: 'projects.index',
+                    name: 'Posts'
+                },
+            ]
+        }
+    },
 }
 </script>
-<style lang="scss" scoped>
-     @use '../styles/partials/mixins.scss' as*;
-     @use '../styles/partials/variables.scss' as*; 
+<style lang="scss">
+    nav{
+        width: 100%;
+        padding: 1rem 3rem;
+        background-color: rgb(232, 232, 232);
+        color: black;
+
+        ul{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            font-size: 2rem;
+            list-style-type: none;
+
+            li{
+                margin-right: 2rem;
+                a {
+                    text-decoration: none;
+                    color: black;
+                }
+            }
+        }
+    }
 </style>
