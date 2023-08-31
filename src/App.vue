266d6dd70a2c2541ad431fbf76373de7b6358ea6
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-31 16:44:17 +0200

    added router-view component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <script>
   import AppFooter from './components/AppFooter.vue';
   import AppHeader from './components/AppHeader.vue';
-  import AppMain from './components/AppMain.vue';
+  
 
   export default{
      name:'App',
      components:{
         AppHeader,
         AppFooter,
-        AppMain
      }
   }
 </script>
 
 <template>
   <AppHeader/>
-  <AppMain/>
+  <router-view></router-view>
   <AppFooter/>
 </template>
 
