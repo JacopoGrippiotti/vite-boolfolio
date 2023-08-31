3dc881d4801f06b8e95f5d30a6291929a84d0826
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-08-31 16:44:44 +0200

    added router.js file

---
+++ src/router.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import AboutUs from './pages/AboutUs.vue';
+import HomePage from './pages/HomePage.vue';
+import Projects from './pages/Projects.vue';
+import ProjectShow from './pages/ProjectShow.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes : [
+        {
+            path: '/',
+            name : 'homepage',
+            component : HomePage,
+        },
+        {
+            path: '/about-us',
+            name : 'about',
+            component : AboutUs,
+        },
+        {
+            path: '/projects',
+            name : 'projects.index',
+            component : Projects,
+        },
+        {
+            path: '/projects/:slug',
+            name : 'projects.show',
+            component : ProjectShow,
+        },
+    ]
+})
+
+export { router };
